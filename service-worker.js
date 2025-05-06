b7ee5170d6ca4e8d32dd707bda4367b830fe542e
Constantin Robert-Andrei <129685219+misterbobert@users.noreply.github.com>
2025-05-06 15:51:45 +0300

    mmm

--- service-worker.js
+++ service-worker.js
@@ -61,4 +61,9 @@ self.addEventListener('install', event => {
         .then(() => self.skipWaiting())
     );
   });
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(() => console.log('SW registered'))
+      .catch(console.error);
+  }
   
